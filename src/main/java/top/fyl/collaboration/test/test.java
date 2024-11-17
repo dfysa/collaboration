5692c1ad98e3f952097784772afadef19bdd0de2
blackcat <123@fox.mail>
2024-11-17 21:21:45 +0800

    完成支付功能

--- src/main/java/top/fyl/collaboration/test/test.java
+++ src/main/java/top/fyl/collaboration/test/test.java
@@ -7,6 +7,6 @@ package top.fyl.collaboration.test;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("欢迎使用我们的系统！"); // 公共输出
+        System.out.println("支付功能已启用！");
     }
 }
