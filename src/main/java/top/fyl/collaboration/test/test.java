beedd753a9e2a3c015b594f6ab3c855882a10107
dfysa <846241197@qq.com>
2024-11-17 21:02:33 +0800

    项目初始化

---
+++ src/main/java/top/fyl/collaboration/test/test.java
@@ -0,0 +1,12 @@
+package top.fyl.collaboration.test;
+
+/**
+ * @author dfysa
+ * @data 2024/11/17 下午8:54
+ * @description
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("欢迎使用我们的系统！"); // 公共输出
+    }
+}
---
+++ src/test/java/top/fyl/collaboration/CollaborationApplicationTests.java
@@ -0,0 +1,13 @@
+package top.fyl.collaboration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
